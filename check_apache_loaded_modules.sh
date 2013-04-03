5412f1f5ce5a96e809f063f7d75fab28a60e4379
Tim Stoop <tim@kumina.nl>
2013-04-03 14:42:34 +0200

    Package the correct version of the script...

--- check_apache_loaded_modules.sh
+++ check_apache_loaded_modules.sh
@@ -16,15 +16,37 @@ if [ `/usr/bin/whoami` != 'root' ]; then
 	exit 3
 fi
 
-# Order is important for these two
-/usr/sbin/apache2ctl -M 2>/dev/null | /usr/bin/awk '/\(shared\)/ { print $1 }' | /usr/bin/xargs -l -i /bin/grep {} /etc/apache2/mods-enabled/*.load | /usr/bin/awk '{ print $NF }' | /usr/bin/xargs -l /usr/bin/dpkg -S | /bin/grep -q ^dpk
-STATUSCODE=`echo $?`
+STATE=4
+CRIT_MODULES=''
+WARN_MODULES=''
+LOADED_SHARED_MODULES=`/usr/sbin/apache2ctl -M 2>/dev/null | /usr/bin/awk '/\(shared\)/ { print $1 }'`
+for module in $LOADED_SHARED_MODULES; do
+	if [ $STATE -eq 4 ]; then STATE=0; fi
+	ON_DISK=`/bin/grep $module /etc/apache2/mods-enabled/*.load | /usr/bin/awk '{ print $NF }'`
+	# Check whether we got a response
+	if [ "$ON_DISK" != "" ]; then
+		IN_PACKAGE=`/usr/bin/dpkg -S $ON_DISK`
+		if [ "$IN_PACKAGE" != "" ]; then
+			# All is fine
+			continue
+		else
+			if [ $STATE -lt 2 ]; then STATE=2; fi
+			CRIT_MODULES="$CRIT_MODULES$ON_DISK;"
+		fi
+	else
+		if [ $STATE -lt 1 ]; then STATE=1; fi
+		WARN_MODULES="$WARN_MODULES$module"
+	fi
+done
 
-if [ $STATUSCODE -eq 1 ]; then
+if [ $STATE -eq 0 ]; then
 	echo 'APACHE OK, no unknown modules found'
 	exit 0
-elif [ $STATUSCODE -eq 0 ]; then
-	echo "APACHE CRITICAL, foreign module found: $(/usr/sbin/apache2ctl -M 2>/dev/null | /usr/bin/awk '/\(shared\)/ { print $1 }' | /usr/bin/xargs -l -i /bin/grep {} /etc/apache2/mods-enabled/*.load | /usr/bin/awk '{ print $NF }' | /usr/bin/xargs -l /usr/bin/dpkg -S | /bin/grep ^dpkg)"
+elif [ $STATE -eq 1 ]; then
+	echo "APACHE WARNING, following modules not found in config: $WARN_MODULES"
+	exit 1
+elif [ $STATE -eq 2 ]; then
+	echo "APACHE CRITICAL, foreign module found: $CRIT_MODULES"
 	exit 2
 else
 	echo "APACHE UNKNOWN, something went wrong"
