8d0f06dac53707ebd2860cd494c57a24078f987f
Rutger Spiertz <rutger@kumina.nl>
2013-07-18 15:19:34 +0200

    Update.

--- check_http_port_url_autostart.sh
+++ check_http_port_url_autostart.sh
@@ -7,7 +7,7 @@ then
 fi
 
 if [ -f $1 ]; then
-	/usr/lib/nagios/plugins/check_http -H 127.0.0.1 -p $2 -u $3 -e $4
+	/usr/lib/nagios/plugins/check_http -H 127.0.0.1 -p $2 -u $3 -e $4 -t 20
 else
 	echo "OK - Not configured to start"
 	exit 0
