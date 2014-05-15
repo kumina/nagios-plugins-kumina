497351e0f917ed81330f63d6a45d07cb1acbab2a
Tim Stoop <tim@kumina.nl>
2014-05-15 14:18:19 +0200

    Cassandra changed nodetool ring output again in 1.2.

--- check_cassandra.sh
+++ check_cassandra.sh
@@ -4,33 +4,19 @@ IPs="`ip add | awk '/inet/ { sub(/\/.*/, "", $2);  print "^" $2 " "}'`"
 msg="CASSANDRA"
 cassandra_status="$(/usr/bin/nodetool ring -h $(hostname) -p 7199 | grep "${IPs}")"
 
-cassandra_major_version=`/usr/bin/dpkg -l cassandra | /usr/bin/awk '{ if ($1 ~/^ii/) { print $3 } }' | /usr/bin/cut -c 1-1`
+#cassandra_major_version=`/usr/bin/dpkg -l cassandra | /usr/bin/awk '{ if ($1 ~/^ii/) { print $3 } }' | /usr/bin/cut -c 1-1`
 
-if [ $cassandra_major_version -lt 1 ]; then
-	case `echo $cassandra_status | /usr/bin/awk '{print $3}'` in
-		"Normal")
-			msg="${msg} OK:"
-			exit_code=0
-			;;
-		*)
-			msg="${msg} CRITICAL:"
-			exit_code=2
-			;;
-	esac
-	msg="${msg} `echo ${cassandra_status} | /usr/bin/awk '{print $2,$3}'`"
-else
-	case `echo $cassandra_status | /usr/bin/awk '{print $5}'` in
-		"Normal")
-			msg="${msg} OK:"
-			exit_code=0
-			;;
-		*)
-			msg="${msg} CRITICAL:"
-			exit_code=2
-			;;
-	esac
-	msg="${msg} `echo ${cassandra_status} | /usr/bin/awk '{print $4,$5}'`"
-fi
+case `echo $cassandra_status | /usr/bin/awk '{print $4}'` in
+	"Normal")
+		msg="${msg} OK:"
+		exit_code=0
+		;;
+	*)
+		msg="${msg} CRITICAL:"
+		exit_code=2
+		;;
+esac
+msg="${msg} `echo ${cassandra_status} | /usr/bin/awk '{print $3,$4}'`"
 
 echo "${msg}"
 exit $exit_code
