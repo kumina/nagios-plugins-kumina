f4e83b6b90f1c5593d567336fd520ffe6514fcd5
Tim Stoop <tim@kumina.nl>
2014-07-10 13:24:22 +0200

    Add caching to check_glassfish.

--- check_glassfish.sh
+++ check_glassfish.sh
@@ -7,7 +7,19 @@ if [ -z $NAME ]; then
         exit 3
 fi
 
-FULLVALUE=`su glassfish -c '/opt/glassfish-3.1.2.2/glassfish/bin/asadmin --port 8048 list-instances' | grep -e "^${NAME}\b" | awk '{$1=""; print $0}'`
+CACHEFILE=/var/cache/glassfish-list-instances
+CACHEDIRNAME=`dirname $CACHEFILE`
+CACHEFILENAME=`basename $CACHEFILE`
+if [ `find $CACHEDIRNAME -name $CACHEFILENAME -mmin 0.5 | wc -l` -eq 0 ]; then
+        # This can take a few seconds, let's use a semaphore to make sure no other process is doing this
+        if [ ! -f "${CACHEFILE}.lock" ]; then
+                touch "${CACHEFILE}.lock"
+                # Run the command to refresh the cache
+                su glassfish -c '/opt/glassfish-3.1.2.2/glassfish/bin/asadmin --port 8048 list-instances' > $CACHEFILE
+                rm "${CACHEFILE}.lock"
+        fi
+fi
+FULLVALUE=`cat $CACHEFILE | grep -e "^${NAME}\b" | awk '{$1=""; print $0}'`
 RUNNING=`echo $FULLVALUE | cut -c1-7`
 
 # Do the actual check
