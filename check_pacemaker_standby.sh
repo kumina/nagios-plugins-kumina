f06e5c93ebbdd49e4168b7d828232f4b248052af
Rutger Spiertz <rutger@kumina.nl>
2016-03-07 13:20:04 +0100

    Update check_pacemaker_standby.

--- check_pacemaker_standby.sh
+++ check_pacemaker_standby.sh
@@ -9,10 +9,10 @@ EXIT_U=3
 
 msg="PACEMAKER STANDBY"
 
-if $(echo "$out" | grep -q 'standby: on'); then
+if $(echo "$out" | grep -q 'standby[:=] \?on'); then
 	echo "$msg CRITICAL: $node is in standby"
 	exit $EXIT_C
-elif $(echo "$out" | grep -q 'standby: off'); then
+elif $(echo "$out" | grep -q 'standby[:=] \?off'); then
 	echo "$msg OK: $node is not in standby"
 	exit $EXIT_OK
 else
